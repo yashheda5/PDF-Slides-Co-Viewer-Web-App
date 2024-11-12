8f4a7ba1b68f513097f73023ee85ca6e728e9aff
yash heda <yashheda5@outlook.com>
2024-11-12 13:32:51 +0530

    c6

---
+++ my-app-backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const cors = require("cors");
+
+const io = new Server(server,{
+    cors:{
+        origin: 'https://pdf-slides-co-viewer-web-app.vercel.app'
+    }
+});
+
+app.use(express.json());
+app.use(cors({
+    origin: 'https://pdf-slides-co-viewer-web-app.vercel.app'
+}))
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+io.on('connection', (socket) => {
+    console.log(`New client connected: ${socket.id}`);
+
+    socket.on('pageChange', (pageNumber) => {
+      console.log(`Page change event received: ${pageNumber}`);
+      socket.broadcast.emit('pageChange', pageNumber);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+
+
+server.listen(3001, ()=>{
+    console.log("listening on port 3001");
+})
