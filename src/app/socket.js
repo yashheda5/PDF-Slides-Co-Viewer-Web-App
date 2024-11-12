8eb8adaba98a3851e80060fbf135c5497cd6f489
yash heda <yashheda5@outlook.com>
2024-11-12 22:21:02 +0530

    c10

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-const URL = 'http://localhost:3001'
+const URL = 'https://pdf-slides-co-viewer-web-app-wqee-backend-2gexkrl57.vercel.app/'
 
 export const socket = io(URL,{
     autoConnect: false
